601abc8a82a3bbc876df536745dcb725aac2dae2
nguyendat1811884 <nguyendat1811884@users.noreply.github.com>
2021-05-15 20:41:21 +0700

    add view humidity info
    display current humidity, history of humidity

--- app/src/main/java/com/example/gardbot/ViewInfomation/ViewHumidityInfoActivity.kt
+++ app/src/main/java/com/example/gardbot/ViewInfomation/ViewHumidityInfoActivity.kt
@@ -2,11 +2,78 @@ package com.example.gardbot.ViewInfomation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.ListView
+import android.widget.TextView
+import com.example.gardbot.Adapter.CustomSoilDetailAdapter
+import com.example.gardbot.Model.SoilHistory
+import com.example.gardbot.Model.SoilSensor
 import com.example.gardbot.R
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class ViewHumidityInfoActivity : AppCompatActivity() {
+
+    var humidityHistoryList = ArrayList<SoilHistory>()
+    var select : SoilHistory? = null
+    var selectPosition : Int? = null
+    lateinit var adapter : CustomSoilDetailAdapter
+    lateinit var listViewHumidityHistory : ListView
+
+    val database = Firebase.database
+    val myRef = database.getReference().child("history").child("humidity")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_humidity_info)
+
+        var textCurHumidity = findViewById<TextView>(R.id.textCurHumidity)
+        database.getReference().child("sensor")
+            .child("rtt").child("1").child("humidity").addValueEventListener(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    textCurHumidity.text = snapshot.value.toString() + "%"
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                }
+            })
+
+
+        listViewHumidityHistory = findViewById<ListView>(R.id.listViewHumidityHistory)
+        adapter = CustomSoilDetailAdapter(this, R.layout.custom_simple_list_element, humidityHistoryList)
+        listViewHumidityHistory.adapter = adapter
+
+        myRef.addChildEventListener(object : ChildEventListener{
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                var time = snapshot.key
+                var value = snapshot.child("value").getValue(String::class.java)
+                humidityHistoryList.add(SoilHistory(time!!, value!!))
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                humidityHistoryList.clear()
+                myRef.removeEventListener(this)
+                myRef.addChildEventListener(this)
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+                humidityHistoryList.clear()
+                myRef.removeEventListener(this)
+                myRef.addChildEventListener(this)
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
     }
 }
--- app/src/main/java/com/example/gardbot/ViewInfomation/ViewInfomationActivity.kt
+++ app/src/main/java/com/example/gardbot/ViewInfomation/ViewInfomationActivity.kt
@@ -5,12 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import com.example.gardbot.R
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class ViewInfomationActivity : AppCompatActivity() {
     lateinit var textViewDetail_soil : TextView;
     lateinit var textViewDetail_humidity : TextView;
     lateinit var textViewDetail_temperator : TextView;
     lateinit var textViewDetail_pump : TextView;
+    val database = Firebase.database
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +26,45 @@ class ViewInfomationActivity : AppCompatActivity() {
         textViewDetail_humidity = findViewById<TextView>(R.id.textViewDetail_Humidity);
         textViewDetail_temperator = findViewById<TextView>(R.id.textViewDetail_Temperature);
         textViewDetail_pump = findViewById<TextView>(R.id.textViewDetail_Pump);
+
+        var textCurHumidity = findViewById<TextView>(R.id.textView_CurrentHumidity)
+        database.getReference().child("sensor")
+            .child("rtt").child("1").child("humidity").addValueEventListener(object :
+                ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    textCurHumidity.text = snapshot.value.toString() + "%"
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                }
+            })
+
+        var textCurTemperature = findViewById<TextView>(R.id.textView_CurrentTemperature)
+        database.getReference().child("sensor")
+            .child("rtt").child("1").child("temperature").addValueEventListener(object :
+                ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    textCurTemperature.text = snapshot.value.toString() + "°C"
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                }
+            })
+
+        var textCurActivePump = findViewById<TextView>(R.id.textView_CurrentActivePump)
+        database.getReference().child("pump").addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    var num_active = 0;
+                    snapshot.children.forEach {
+                        if (it.child("waterLevel").value != "0")
+                            num_active = num_active + 1;
+                    }
+                    textCurActivePump.text = "active: " + num_active.toString() + "/" +snapshot.getChildrenCount().toString()
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                }
+            })
 
         textViewDetail_soil.setOnClickListener{
             moveToSoilInfo();
