f97dfbef3495d7a82ca831bc33136e54b190321a
kdat0310 <nguyenkimdat03@gmail.com>
2021-05-15 20:58:44 +0700

    add pump control
    Vẫn chưa add 2 cái pump vô 1 sensor được...

---
+++ app/src/main/java/com/example/gardbot/ActivityPump.java
@@ -0,0 +1,39 @@
+package com.example.gardbot;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ActivityPump extends AppCompatActivity {
+    RecyclerView recview;
+    myAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_controlpump);
+        recview=(RecyclerView)findViewById(R.id.recview);
+        recview.setLayoutManager(new LinearLayoutManager(this));
+
+        FirebaseRecyclerOptions<model> options =
+                new FirebaseRecyclerOptions.Builder<model>().setQuery(FirebaseDatabase.getInstance().getReference().child("pump"), model.class).build();
+        adapter= new myAdapter(options);
+        recview.setAdapter(adapter);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+}
