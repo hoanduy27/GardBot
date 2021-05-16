5a40c6130a81e15a5be193d08a2240d0dcedd9bc
Duy306 <67038741+Duy306@users.noreply.github.com>
2021-05-17 01:45:42 +0700

    success post to flask

--- app/src/main/java/com/example/gardbot/RetrofitClient.kt
+++ app/src/main/java/com/example/gardbot/RetrofitClient.kt
@@ -7,31 +7,30 @@ import retrofit2.Response
 import retrofit2.Retrofit
 
 class RetrofitClient {
-    private var BASE_URL = "http://127.0.0.1:5000/"
+    private var BASE_URL = "http://10.0.2.2:5000/"
     private var retrofit: Retrofit? = null
 
-    public fun getClient(url: String): Retrofit {
-        retrofit = Retrofit.Builder()
+    public fun getClient(url: String) {
+        this.retrofit = Retrofit.Builder()
             .baseUrl(url)
             .build()
-        return retrofit!!
     }
 
     public fun uploadValue(value: String) {
-        val flaskMqttApi = retrofit?.create(FlaskMqttApi::class.java)
+        val flaskMqttApi = this.retrofit?.create(FlaskMqttApi::class.java)
         val call = flaskMqttApi?.uploadValue(value)
         call?.enqueue(object: Callback<Void> {
             override fun onResponse(call: Call<Void>, response: Response<Void> )
             {
-                // Xử lý response server trả về khi sign up ở đây
+                // Xử lý response server trả về
             }
             override fun onFailure(call: Call<Void>?, t: Throwable?) {
-                Log.v("retrofit", "call failed")
+                Log.v("retrofit", t.toString())
             }
         })
     }
 
     init {
-        getClient(BASE_URL)
+        getClient(this.BASE_URL)
     }
 }
