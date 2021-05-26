bff9e0bb8e8a3540edb67e0ffe5af3dd6f03bc7e
nguyendat1811884 <nguyendat1811884@users.noreply.github.com>
2021-05-26 13:38:16 +0700

    update View Pump Info UI

--- app/src/main/java/com/example/gardbot/Model/Pump.kt
+++ app/src/main/java/com/example/gardbot/Model/Pump.kt
@@ -1,6 +1,7 @@
 package com.example.gardbot.Model
 
 class Pump {
+    var auto : String? = null
     var name : String? = null
     var soilMoistureID : String? = null
     var waterLevel : String? = null
--- app/src/main/java/com/example/gardbot/ViewInfomation/ViewPumpInfoActivity.kt
+++ app/src/main/java/com/example/gardbot/ViewInfomation/ViewPumpInfoActivity.kt
@@ -2,14 +2,69 @@ package com.example.gardbot.ViewInfomation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ListView
+import com.example.gardbot.Adapter.CustomPumpInfoAdapter
+import com.example.gardbot.Adapter.CustomSoilDetailAdapter
+import com.example.gardbot.Model.Pump
+import com.example.gardbot.Model.SoilHistory
+import com.example.gardbot.Model.SoilSensor
 import com.example.gardbot.R
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class ViewPumpInfoActivity : AppCompatActivity() {
 
+    var pumpList = ArrayList<Pump>()
+    var select : Pump? = null
+    var selectPosition : Int? = null
+    lateinit var adapter : CustomPumpInfoAdapter
+    lateinit var listViewPump : ListView
+
+    val database = Firebase.database
+    val myRef = database.getReference().child("pump")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_pump_info)
 
+        listViewPump = findViewById<ListView>(R.id.listViewPump)
+        adapter = CustomPumpInfoAdapter(this, R.layout.custom_listview_pump, pumpList)
+        listViewPump.adapter = adapter
+
+        myRef.addChildEventListener(object : ChildEventListener{
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                var cur_pump : Pump = snapshot.getValue(Pump::class.java)!!
+                pumpList.add(cur_pump)
+                Log.d("-------------pump auto--------------", cur_pump.auto!!.toString())
+                Log.d("-------------pump name--------------", cur_pump.name!!.toString())
+                Log.d("-------------pump soil--------------", cur_pump.soilMoistureID!!.toString())
+                Log.d("-------------pump water level--------------", cur_pump.waterLevel!!.toString())
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                pumpList.clear()
+                myRef.removeEventListener(this)
+                myRef.addChildEventListener(this)
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+//                pumpList.clear()
+//                myRef.removeEventListener(this)
+//                myRef.addChildEventListener(this)
+//                adapter.notifyDataSetChanged()
+            }
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
     }
 }
