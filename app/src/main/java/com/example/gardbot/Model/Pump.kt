20ea3733505c6bbea87cd017cf5b66996c5b71e0
nguyendat1811884 <nguyendat1811884@users.noreply.github.com>
2021-06-11 20:56:15 +0700

    Update Pump.kt

--- app/src/main/java/com/example/gardbot/Model/Pump.kt
+++ app/src/main/java/com/example/gardbot/Model/Pump.kt
@@ -1,8 +1,20 @@
-package com.example.gardbot.Model
+package com.example.gardbot.model
 
-class Pump {
-    var auto : String? = null
+import android.os.Parcelable
+import java.io.Serializable
+
+class Pump : Serializable{
     var name : String? = null
     var soilMoistureID : String? = null
     var waterLevel : String? = null
+    var auto : String? = null
+
+    constructor(){}
+    constructor(_name: String, _soilMoistureiD : String, _waterLevel: String, _auto: String){
+        name = _name
+        soilMoistureID = _soilMoistureiD
+        waterLevel = _waterLevel
+        auto = _auto
+    }
+
 }
